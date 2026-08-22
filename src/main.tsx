1531dd5a7b0a2f03fa2ddcbe863b0bd2d3670bce
Joliel21 <2emailjolie@gmail.com>
2026-08-22 18:54:16 -0500

    Use RRM runtime in standalone reader

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@/app/App';
+import { installRrmRuntime } from '@/app/config/rrm-runtime';
 import '@/styles/index.css';
+
+installRrmRuntime();
 
 const rootElement = document.getElementById('root');
 
